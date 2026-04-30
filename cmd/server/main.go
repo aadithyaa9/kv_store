e357bc2f8b45e360476b9d3378b8a9f5d7094cc2
aadithyaa9 <aadiramki01@gmail.com>
2026-04-30 14:13:28 +0530

    Main method to handle the necessary requests from node with goroutines

---
+++ cmd/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/aadithyaa9/kv_store/internal/config"
+	"github.com/aadithyaa9/kv_store/internal/logger"
+	"github.com/aadithyaa9/kv_store/internal/node"
+	"github.com/aadithyaa9/kv_store/internal/transport"
+)
+
+func main() {
+	logger.Init()
+	cfg := config.Load()
+
+	addr := "localhost:" + cfg.Port
+	n := node.New(addr, cfg.Peers)
+
+	handler := transport.NewHandler(n)
+
+	server := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: handler,
+	}
+
+	go func() {
+		log.Printf("Node running at %s", addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// graceful shutdown
+	
+}
